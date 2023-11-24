7f189e0bb3f4308fe53c19e0ff0f44db8bc7acb5
010binary <augustine32fg@gmail.com>
2023-11-24 06:35:09 +0100

    adding the save tab and delete button as well as there functionality

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,11 +1,18 @@
-
 // Declaring Variables
 const inputfield = document.getElementById('input-el');
 const button = document.getElementById('input-btn');
+const deleteAll = document.getElementById('delete-all');
+const saveTab = document.getElementById('save-tab');
 const ulEl = document.getElementById('unorderedList');
 const empty = document.querySelector('.nothing');
 let links = [];
 let linksFromLocalStorage = JSON.parse(localStorage.getItem('Savings'))
+
+const tabs = [
+    {
+        url: 'https://github.com/010binary'
+    }
+]
 
 // Function to toggle visibility of empty element
 const hideToggler = () => {
@@ -18,22 +25,22 @@ const hideToggler = () => {
 
 //Function to a Handle Rendering the element
 const render = () => {
-   
+
     let lists = '';
 
-        for (let i = 0; i < links.length; i++) {
-            lists = `<li>
+    for (let i = 0; i < links.length; i++) {
+        lists = `<li>
                     <a href="${links[i]}" target="_blank" >
                         ${links[i]}
                     </a>
                 </li>`
-        }
-        // rendering the list element
-        
-        ulEl.innerHTML += lists;
+    }
+    // rendering the list element
 
-        //removeing the hide class from the display
-        hideToggler();
+    ulEl.innerHTML += lists;
+
+    //removeing the hide class from the display
+    hideToggler();
 }
 
 //Local Storage
@@ -42,7 +49,7 @@ if (linksFromLocalStorage) {
     render()
 
 }
-
+// The Button Reaction function
 function buttonAction() {
 
     if (inputfield.value === '') {
@@ -58,7 +65,25 @@ function buttonAction() {
 // Event Listener for the Button
 button.addEventListener('click', () => buttonAction());
 
+// This is to ensure the values added last are rendered to the container
 render()
 
 // To show the empty div when the code starts
 hideToggler();
+
+// To Clear all the add
+deleteAll.addEventListener('click', () => {
+    localStorage.clear()
+    location.reload()
+})
+
+saveTab.addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+
+    }) //tring to get the current chrome tab
+
+    let tabb = tabs[0].url
+    links.push(tabb)
+    localStorage.setItem('Savings', JSON.stringify(links))
+    render()
+})
